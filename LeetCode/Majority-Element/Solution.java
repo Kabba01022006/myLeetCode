93c6e843e8f37965d6e1b015252ad0e22f90cf5c
shobhitkabba <125171824+Kabba01022006@users.noreply.github.com>
2026-08-26 01:23:38 +0530

    Add solution for Majority Element

--- LeetCode/Majority-Element/Solution.java
+++ LeetCode/Majority-Element/Solution.java
@@ -1,15 +1,16 @@
 1class Solution {
 2    public int majorityElement(int[] nums) {
-3        int tar=nums.length/2;
-4        HashMap<Integer,Integer> map = new HashMap<>();
-5        for(int ele:nums){
-6            map.put(ele,map.getOrDefault(ele,0)+1);
-7            if(map.get(ele)>tar) return ele;
-8            // map.put(ele,map.getOrDefault(ele,0)+1);
-9        }
-10        // for(int ele:map.keySet()){
-11        //     if(map.get(ele)>tar) return ele;
-12        // }
-13        return -1;
-14    }
-15}
+3        int n=nums.length;
+4        int currElem=nums[0];
+5        int count=1;
+6        for(int i=1;i<n;i++){
+7            if(currElem==nums[i]) count++;
+8            else count--;
+9            if (count==0){
+10                currElem=nums[i];
+11                count=1;
+12            }
+13        }
+14        return currElem;
+15    }   
+16}
