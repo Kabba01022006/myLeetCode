7b307f1ba44a5cb8e70ed3c45e1ebfe87a00adf4
shobhitkabba <125171824+Kabba01022006@users.noreply.github.com>
2026-08-26 20:54:15 +0530

    Add solution for Subarray Product Less Than K

--- LeetCode/Subarray-Product-Less-Than-K/Solution.java
+++ LeetCode/Subarray-Product-Less-Than-K/Solution.java
@@ -1,29 +1,23 @@
 1class Solution {
 2    public int numSubarrayProductLessThanK(int[] nums, int k) {
-3        //handle the edge case
-4        if(nums.length==1){
-5            if(k>nums[0]) return 1;
-6            else return 0;
-7        }
+3        if(nums.length==1){
+4            if(nums[0]>=k) return 0;
+5            else return 1;
+6        }
+7        // 10 5 2 6 
 8        int left=0;
 9        int right=0;
 10        int pro=1;
 11        int count=0;
 12        while(right<nums.length){
-13
-14            pro=pro*nums[right];
-15
-16            while(pro>=k){
-17                pro=pro/nums[left];
-18                left++;
-19            }
-20            count = count + (right-left+1); //very very important (calculates and adds the number of subarrays at each point of our valid window)
-21
-22            // counts the number of valid subarrays ending at 'right'
-23
-24            //everyTime a valid window is found, the number of subarrays it adds == len of that window
-25            right++;
-26        }
-27        return count;
-28    }
-29}
+13            pro=pro*nums[right];
+14            while(pro>=k){
+15                pro=pro/nums[left];
+16                left++;
+17            }
+18            count+=right-left+1;
+19            right++;
+20        }
+21        return count;
+22    }
+23}
