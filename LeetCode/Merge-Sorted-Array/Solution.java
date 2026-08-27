2d0ff20b5bbc7591643886e0c27c28da2e03a030
shobhitkabba <125171824+Kabba01022006@users.noreply.github.com>
2026-08-27 09:07:35 +0530

    Add solution for Merge Sorted Array

--- LeetCode/Merge-Sorted-Array/Solution.java
+++ LeetCode/Merge-Sorted-Array/Solution.java
@@ -1,32 +1,28 @@
 1class Solution {
 2    public void merge(int[] nums1, int m, int[] nums2, int n) {
-3        int[] temp = new int[m];
-4        for(int i=0;i<m;i++){
-5            temp[i] = nums1[i];
-6        }
-7        int i=0,j=0,k=0;
-8        while(i<m && j<n){
-9            if(temp[i]<nums2[j]){
-10                nums1[k] = temp[i];
-11                i++; 
-12            }
-13            else{
-14                nums1[k] = nums2[j];
-15                j++;
-16            }
-17            k++;
-18        }
-19        while(i<m){
-20            nums1[k] = temp[i];
-21            i++;
-22            k++;
-23        }
-24        while(j<n){
-25            nums1[k] = nums2[j];
-26            j++;
-27            k++;
-28        } 
-29    }
-30}
-31
-32// no need for return ; statement when its void , just simply skip it.
+3        // start  backwards
+4        int len = m + n;
+5
+6        int i=m-1;
+7        int j=n-1;
+8        int k=len-1;
+9
+10        while(j>=0 && i>=0){
+11            if(nums2[j]>=nums1[i]){
+12                nums1[k]=nums2[j];
+13                j--;
+14                k--;
+15            }
+16            else if ( nums1[i]>nums2[j]){
+17                nums1[k]=nums1[i];
+18                i--;
+19                k--;
+20            }
+21        }
+22        while(j>=0){
+23            nums1[k]=nums2[j];
+24            j--;
+25            k--;
+26        }
+27    }
+28}
