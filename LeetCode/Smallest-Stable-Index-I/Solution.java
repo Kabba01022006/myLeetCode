47a206abcf8a0439e1feeeaf1c08c74da9e58458
shobhitkabba <125171824+Kabba01022006@users.noreply.github.com>
2026-09-05 01:03:03 +0530

    Add solution for Smallest Stable Index I

--- LeetCode/Smallest-Stable-Index-I/Solution.java
+++ LeetCode/Smallest-Stable-Index-I/Solution.java
@@ -1,29 +1,20 @@
 1class Solution {
 2    public int firstStableIndex(int[] nums, int k) {
-3        // make two arrays as same as the length of nums 
-4
-5        int[] maxArr = new int[nums.length];
-6        int[] minArr = new int[nums.length];
-7        
-8        int max = Integer.MIN_VALUE;
-9        int min = Integer.MAX_VALUE;
-10
-11        for(int i=0;i<nums.length;i++){
-12            if(nums[i]>max){
-13                max=nums[i];
-14            }
-15            maxArr[i]=max;
+3        // make one extra array
+4        int[] minArr = new int[nums.length];
+5        int max = Integer.MIN_VALUE;
+6        int min = Integer.MAX_VALUE;
+7        for(int i=nums.length-1;i>=0;i--){
+8            if(nums[i]<min){
+9                min=nums[i];
+10            }
+11            minArr[i]=min;
+12        }
+13        for(int i=0;i<nums.length;i++){
+14            max = Math.max(max,nums[i]);
+15            if(max-minArr[i]<=k) return i;
 16        }
-17        for(int i=nums.length-1;i>=0;i--){
-18            if(nums[i]<min){
-19                min=nums[i];
-20            }
-21            minArr[i]=min;
-22        }
-23        for(int i=0;i<nums.length;i++){
-24            if(maxArr[i]-minArr[i]<=k) return i;
-25        }
-26        return -1;
-27    }
-28}
-29
+17        return -1;
+18    }
+19}
+20
