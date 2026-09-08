7d29cb588e4838f661447b5199e634aaa7c5595c
shobhitkabba <125171824+Kabba01022006@users.noreply.github.com>
2026-09-08 12:01:17 +0530

    Add solution for Count Commas in Range II

--- LeetCode/Count-Commas-in-Range-II/Solution.java
+++ LeetCode/Count-Commas-in-Range-II/Solution.java
@@ -1,34 +1,28 @@
 1class Solution {
 2    public long countCommas(long n) {
-3        long countCom=0;
-4        int len = countDigit(n);
-5        while(len>=4){
-6            //5,200,000 to 1,000,000 ,
-7            // thats 5,200,000 - 999,999 + 1 commas
-8            int commas = (len-1)/3;
-9            long nums = n - exp(10,len-1) + 1;
-10            countCom += nums*commas;
-11            n = exp(10,len-1)-1;
-12            len--;
-13        }
-14        
-15        return countCom;
-16    }
-17
-18    public int countDigit(long n){
-19        int count=0;
-20        while(n!=0){
-21            count++;
-22            n=n/10;
-23        }
-24        return count;
-25    }
-26
-27    public long exp(int base, int pow){
-28        long ans = 1 ;
-29        for(int i=0;i<pow;i++){
-30            ans = ans * base ; 
-31        }
-32        return ans;
-33    }
-34}
+3        long total = 0;
+4        int digits = countDigits(n);
+5        long power = 1;
+6        for (int i = 1; i < digits; i++) {
+7            power *= 10;
+8        }
+9        while (digits >= 4) {
+10            int commas = (digits - 1) / 3;
+11            long count = n - power + 1;
+12            total += count * commas;
+13            n = power - 1;
+14            power /= 10;
+15            digits--;
+16        }
+17        return total;
+18    }
+19
+20    public int countDigits(long n) {
+21        int digits = 0;
+22        while (n != 0) {
+23            digits++;
+24            n /= 10;
+25        }
+26        return digits;
+27    }
+28}
