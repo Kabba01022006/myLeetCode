c28b92fe0e0b514148f5456f72f7af493b098278
shobhitkabba <125171824+Kabba01022006@users.noreply.github.com>
2026-09-10 20:11:21 +0530

    Add solution for Hand of Straights

---
+++ LeetCode/Hand-of-Straights/Solution.java
@@ -0,0 +1,31 @@
+1class Solution {
+2    public boolean isNStraightHand(int[] nums, int k) {
+3        HashMap<Integer,Integer> map = new HashMap<>();
+4        if(nums.length%k!=0){
+5            return false;
+6        }
+7        for(int i=0;i<nums.length;i++){
+8            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+9        }
+10        Arrays.sort(nums);
+11        for(int i=0;i<nums.length;i++){
+12            if(map.get(nums[i]) <= 0){
+13                continue;
+14            }
+15            int start = nums[i];
+16            for(int j=0;j<k;j++){
+17            if(!map.containsKey(start)){
+18                return false;
+19            }
+20            else if(map.containsKey(start) && map.get(start)<=0){
+21                return false;
+22            }
+23            else{
+24                map.put(start,map.get(start)-1);
+25                start++;
+26            }
+27        }
+28    }
+29    return true;
+30    }
+31}
