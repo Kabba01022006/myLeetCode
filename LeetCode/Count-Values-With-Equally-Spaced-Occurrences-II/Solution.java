1978a5327c884710b8d474e7fb643f70285b618b
shobhitkabba <125171824+Kabba01022006@users.noreply.github.com>
2026-09-12 22:41:59 +0530

    Add solution for Count Values With Equally Spaced Occurrences II

--- LeetCode/Count-Values-With-Equally-Spaced-Occurrences-II/Solution.java
+++ LeetCode/Count-Values-With-Equally-Spaced-Occurrences-II/Solution.java
@@ -12,21 +12,20 @@
 12            }
 13        }
 14        int count=0;
-15        ArrayList<Integer> list = new ArrayList<>();
-16        for(int ele:map.keySet()){
-17            if(map.get(ele).size()>=3){
-18                boolean flag = true;
-19                ArrayList<Integer> innerList = map.get(ele);
-20                int gap = innerList.get(1) - innerList.get(0);
-21                for(int i=1;i<innerList.size();i++){
-22                    if(innerList.get(i)-innerList.get(i-1)!=gap){
-23                        flag=false;
-24                    }
-25                }
-26                if(flag==true) count++;
-27                
-28            }
-29        }
-30        return count;
-31    }
-32}
+15        for(int ele:map.keySet()){
+16            if(map.get(ele).size()>=3){
+17                boolean flag = true;
+18                ArrayList<Integer> innerList = map.get(ele);
+19                int gap = innerList.get(1) - innerList.get(0);
+20                for(int i=1;i<innerList.size();i++){
+21                    if(innerList.get(i)-innerList.get(i-1)!=gap){
+22                        flag=false;
+23                    }
+24                }
+25                if(flag==true) count++;
+26                
+27            }
+28        }
+29        return count;
+30    }
+31}
