c21d9d892ff2bffec416b577d7659d879f83937d
shobhitkabba <125171824+Kabba01022006@users.noreply.github.com>
2026-09-17 14:43:10 +0530

    Add solution for Maximum Points You Can Obtain from Cards

--- LeetCode/Maximum-Points-You-Can-Obtain-from-Cards/Solution.java
+++ LeetCode/Maximum-Points-You-Can-Obtain-from-Cards/Solution.java
@@ -3,23 +3,22 @@
 3        int lsum=0;
 4        int rsum=0;
 5        int maxSum=0;
-6        int sum=lsum+rsum;
-7        int lIdx=k-1;
-8        //first take all from left
-9        for(int i=0;i<=lIdx;i++){
-10            lsum+=arr[i];
-11        }
-12        sum=lsum+rsum;
-13        maxSum = Math.max(sum,maxSum); //left total side sum 
-14        int rIdx=arr.length-1;
-15        while(lIdx>=0){
-16            lsum=lsum-arr[lIdx];
-17            lIdx--;
-18            rsum=rsum+arr[rIdx];
-19            rIdx--;
-20            sum=lsum+rsum;
-21            maxSum = Math.max(sum,maxSum);
-22        }
-23        return maxSum;
-24    }
-25}
+6        int lIdx=k-1;
+7        //first take all from left
+8        for(int i=0;i<=lIdx;i++){
+9            lsum+=arr[i];
+10        }
+11        int sum=lsum+rsum;
+12        maxSum = Math.max(sum,maxSum); //left total side sum 
+13        int rIdx=arr.length-1;
+14        while(rIdx>=arr.length-k){
+15            lsum=lsum-arr[lIdx];
+16            lIdx--;
+17            rsum=rsum+arr[rIdx];
+18            rIdx--;
+19            sum=lsum+rsum;
+20            maxSum = Math.max(sum,maxSum);
+21        }
+22        return maxSum;
+23    }
+24}
