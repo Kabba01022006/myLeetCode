72bfd8c2cc9bf6a268f89162d719b6fc36b72f1b
shobhitkabba <125171824+Kabba01022006@users.noreply.github.com>
2026-09-17 12:18:55 +0530

    Add solution for Permutation in String

--- LeetCode/Permutation-in-String/Solution.java
+++ LeetCode/Permutation-in-String/Solution.java
@@ -1,28 +1,22 @@
 1class Solution {
 2    public boolean checkInclusion(String s1, String s2) {
-3        boolean flag = true;
-4        HashMap<Character,Integer> map = new HashMap<>();
-5        int[] freq = new int[26];
+3        if(s1.length()>s2.length()) return false;
+4        int[] freq = new int[26];
+5        int[] win = new int[26];
 6        for(int i=0;i<s1.length();i++){
-7            char ch = s1.charAt(i);
-8            freq[ch-'a']++;
+7            freq[s1.charAt(i)-'a']++;
+8            win[s2.charAt(i)-'a']++;
 9        }
-10        int left=0;
-11        int right=s1.length()-1;
-12        int[] win = new int[26];
-13        while(right<s2.length()){
-14            for(int i=left;i<=right;i++){
-15                win[s2.charAt(i)-'a']++;
-16            }
-17            if(Arrays.equals(freq,win)){
-18                return true;
-19            }
-20            left++;
-21            right++;
-22            for(int i=0;i<win.length;i++){
-23                win[i]=0;
-24            }
-25        }
-26        return false;
-27    }
-28}
+10        if(Arrays.equals(freq,win)) return true;
+11        int left=0;
+12        int right=s1.length()-1;
+13        while(right<s2.length()-1){
+14            win[s2.charAt(left)-'a']--;
+15            left++;
+16            right++;
+17            win[s2.charAt(right)-'a']++;
+18            if(Arrays.equals(freq,win)) return true;
+19        }
+20        return false;
+21    }
+22}
