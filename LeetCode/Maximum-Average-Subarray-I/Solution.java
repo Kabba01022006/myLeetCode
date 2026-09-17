5f17ac583c67e50d3aa41246d0abc541d6824aaf
shobhitkabba <125171824+Kabba01022006@users.noreply.github.com>
2026-09-17 11:06:31 +0530

    Add solution for Maximum Average Subarray I

--- LeetCode/Maximum-Average-Subarray-I/Solution.java
+++ LeetCode/Maximum-Average-Subarray-I/Solution.java
@@ -1,24 +1,19 @@
 1class Solution {
-2    static {
-3        for (int i = 0; i < 999; i++) {
-4            findMaxAverage(new int[1], 1);
-5        }
-6    }
-7    public static double findMaxAverage(int[] nums, int k) {
-8        double sum = 0 ;
-9        int l=0;
-10        int r=k-1;
-11        for(int i=0;i<=r;i++){
-12            sum+=nums[i];
-13        }
-14        double maxSum=sum;
-15        while(r<nums.length-1){
-16            sum-=nums[l];
-17            l++;
-18            r++;
-19            sum+=nums[r];
-20            maxSum=Math.max(maxSum,sum);
-21        }
-22        return maxSum/k;
-23    }
-24}
+2    public double findMaxAverage(int[] nums, int k) {
+3        int sum = 0 ;
+4        int l=0;
+5        int r=k-1;
+6        for(int i=0;i<=r;i++){
+7            sum+=nums[i];
+8        }
+9        int maxSum=sum;
+10        while(r<nums.length-1){
+11            sum-=nums[l];
+12            l++;
+13            r++;
+14            sum+=nums[r];
+15            maxSum=Math.max(maxSum,sum);
+16        }
+17        return (double)maxSum/k; //or we can simply make maxSum double when declaring
+18    }
+19}
